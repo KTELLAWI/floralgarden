4d75205c2b44fc3dae8b6df66903d29d0053b975
KTELLAWI <koutaiba@msn.com>
2022-09-13 18:40:40 +0300

    chashing is false

--- lib/env.dart
+++ lib/env.dart
@@ -36,9 +36,9 @@ Map<String, dynamic> environment =
 "EnableSkuSearch":true,
 "showStockStatus":true,
 "GridCount":3,
-"isCaching":true,
+"isCaching":false,
 "kIsResizeImage":true,
-"httpCache":true,
+"httpCache":false,
 "DefaultCurrency":{
 "symbol":"QAR",
 "decimalDigits":2,
